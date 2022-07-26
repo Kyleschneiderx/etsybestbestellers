e242018e7549482cec9084a6856fbb78d3ca54df
kyleschneiderx <kyleschneiderx@gmail.com>
2022-07-26 13:48:43 -0700

    fix api

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config').get(process.env.NODE_ENV);
 const SALT_I = 10;
-const generateApiKey = require('generate-api-key')
+const generateApiKey = require('generate-api-key').default
 
 
 
